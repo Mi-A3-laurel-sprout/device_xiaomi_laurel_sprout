025a54517d34873a2f66a34b5bf0808012a69495
Magicxavi <rolfpeterkesche@gmail.com>
2022-11-15 07:37:30 +0200

    laurel_sprout: add camera apps to remove packages
    As we ship Aperture, need to drop standart camera packages from building

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,10 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup Drive
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera Camera2 CarrierSetup Drive
 LOCAL_OVERRIDES_PACKAGES += Maps MyVerizonServices Music MusicFX AudioFX
 LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM Snap
 LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
