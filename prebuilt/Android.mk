e33cf2ba2b2e74debb06a7b3eadedac9a9516c27
anirudhkosgi <kosgi34@gmail.com>
2023-01-08 09:46:21 +0000

    laurel_sprout: Drop packages

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -1,44 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := FirefoxLite
-LOCAL_SRC_FILES := priv-app/FirefoxLite.apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_DEX_PREOPT := false
-LOCAL_OVERRIDES_PACKAGES := Jelly Browser ViaBrowser
-include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX Snap GoogleTTS talkback webview Camera Camera2 Snap
+LOCAL_OVERRIDES_PACKAGES := arcore AudioFX Snap GoogleTTS talkback webview Camera Camera2 Snap
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera Camera2 CarrierSetup Drive
+LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail Maps MyVerizonServices Music MusicFX AudioFX
+LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt SoundPickerPrebuilt SoundAmplifierPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM Snap AndroidAutoStubPrebuilt RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt Flipendo
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := GCamGo
-LOCAL_SRC_FILES := priv-app/GCamGo.apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Snap Camera2
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_DEX_PREOPT := false
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := GalleryGo2
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := priv-app/GalleryGo2.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := GalleryGo Photos Gallery Gallery2
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_DEX_PREOPT := false
-LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
