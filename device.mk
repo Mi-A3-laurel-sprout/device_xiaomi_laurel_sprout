177444d38eaea5360f47dac6bf0bcd62c78ce92e
anirudhkosgi <kosgi34@gmail.com>
2023-02-26 19:40:35 +0100

    laurel_sprout: welcome BlackHole https://github.com/Sangwan5688/BlackHole

--- device.mk
+++ device.mk
@@ -472,3 +472,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1
+
+# BlackHole
+PRODUCT_PACKAGES += \
+    BlackHole
