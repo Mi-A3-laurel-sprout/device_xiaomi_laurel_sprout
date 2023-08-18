b396deff678af615ad5f6601c03dabad2f072b31
anirudhkosgi <kosgi34@gmail.com>
2023-08-18 11:17:16 +0200

    Revert "laurel_sprout: welcome BlackHole"
    This reverts commit 177444d38eaea5360f47dac6bf0bcd62c78ce92e.

--- device.mk
+++ device.mk
@@ -475,7 +475,3 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1
-
-# BlackHole
-PRODUCT_PACKAGES += \
-    BlackHole
